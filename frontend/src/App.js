533011eecc011e1e3b02c14a90e58655c9d5e033
Qiuyun Chen <qiuyunchen@pursuit.org>
2019-04-17 22:12:18 -0400

    added incomplete routes to app

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,25 @@
 import React, { Component } from 'react';
+import {HashRouter, Route, Switch} from 'react-router-dom';
+
+// Containers / Components
+import Header from './components/header';
+import Home from './components/home';
+import Users from './containers/users';
+
 import './App.css';
 
 export default class App extends Component {
   render() {
     return (
-      <div className="App">
-        Hello app
-      </div>
+      <HashRouter>
+        <Route path='/' component={Header}/>
+        <div className='App-body'>
+          <Switch>
+            <Route path='/' exact component={Home}/>
+            <Route path='/users' exact component={Users}/>
+          </Switch>
+        </div>
+      </HashRouter>
     );
   }
 }
