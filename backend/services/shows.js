8a47d93f8d08b9d8f48959025932f18ef640c170
Qiuyun Chen <qiuyunchen@pursuit.org>
2019-04-17 21:17:53 -0400

    added shows services

---
+++ backend/services/shows.js
@@ -0,0 +1,42 @@
+const db = require('./db-connect');
+const ShowsService = {};
+module.exports = ShowsService;
+
+ShowsService.getAllShows = () =>{
+    return db.any('SELECT * FROM shows;');
+}
+
+ShowsService.getShowsByGenreId = (id) =>{
+    const sql = `
+        SELECT * FROM shows
+        WHERE genre_id = $[id];
+    `
+    return db.any(sql, {id});
+}
+
+ShowsService.getShowsByUserId = (id) =>{
+    const sql = `
+        SELECT * FROM shows
+        WHERE user_id = $[id];
+    `
+    return db.any(sql, {id});
+}
+
+ShowsService.getShowById = (id) =>{
+    const sql = `
+        SELECT * FROM shows
+        WHERE id = $[id];
+    `
+    return db.one(sql, {id});
+}
+
+ShowsService.createNewShow = ({title, img_url, user_id, genre_id}) =>{
+    const sql = `
+        INSERT INTO shows
+        (title, img_url, user_id, genre_id)
+        VALUES
+        ($[title], $[img_url], $[user_id], $[genre_id])
+        RETURNING *;
+    `
+    return db.one(sql, {title, img_url, user_id, genre_id});
+}
