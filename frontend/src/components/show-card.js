66559aa65af21d4e10f3b493a2f7bcca0ef8aab7
Qiuyun Chen <qiuyunchen@pursuit.org>
2019-04-20 15:47:46 -0400

    linked shows to show profile page

--- frontend/src/components/show-card.js
+++ frontend/src/components/show-card.js
@@ -1,11 +1,15 @@
 import React from 'react';
+import {Link} from 'react-router-dom';
 import './show-card.css';
 
 export default (props) =>{
     const {id, title, img_url, genre_name} = props;
+
     return <div className='show-card' showid={id}>
-        <img className='show-img' src={img_url} alt={title}></img>
-        <h3>{title}</h3>
-        <p>Genre: {genre_name}</p>
+        <Link to={'/show/'+id} className='no-deco show-link'>
+            <img className='show-img' src={img_url} alt={title}></img>
+            <h1>{title}</h1>
+            <p>Genre: {genre_name}</p>
+        </Link>
     </div>;
 }
