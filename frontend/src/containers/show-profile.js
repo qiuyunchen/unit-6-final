4dc563befcfc1afe403942d8a96729f7e18752e1
Qiuyun Chen <qiuyunchen@pursuit.org>
2019-04-22 21:45:04 -0400

    added top portion of show-profile page

--- frontend/src/containers/show-profile.js
+++ frontend/src/containers/show-profile.js
@@ -1,10 +1,43 @@
 import React from 'react';
+import Axios from 'axios';
+import './show-profile.css';
 
 export default class ShowProfile extends React.Component {
     state = {
-
+        show: {},
+        comment: '',
+        comments: [],
     }
+
+    componentDidMount (){
+        const showId = this.props.match.params.id;
+
+        Axios.get(`http://localhost:5555/shows/${showId}`)
+            .then(res =>{
+                const {show} = res.data;
+                this.setState({show});
+            })
+            .catch( err =>{
+                console.log('get show by show id error!!! ', err);
+            })
+    }
+
     render (){
-        return 'show profile';
+        const {show, comment, comments} = this.state;
+        const {title, img_url, genre_name, username} = show;
+
+        return <>
+        <div className='show-profile-display'>
+            <img src={img_url} alt={title} className='show-profile-img'/>
+            <div className='show-profile-txt'>
+                <h1>{title}</h1>
+                <h2>Genre: {genre_name}</h2>
+            </div>
+        </div>
+        <h1 className='show-profile-txt'>Being watched by {username}</h1>
+        <hr></hr>
+
+        
+        </>;
     }
 }
